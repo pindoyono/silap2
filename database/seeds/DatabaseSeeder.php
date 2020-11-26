9ba882a6a0114ed31fc515590016d53f2c0faea2
Patan Pindoyono <pindoyonopatan@gmail.com>
2020-11-26 17:17:23 +0800

    git

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(RoleTableSeeder::class);
-        $this->call(ProfileTableSeeder::class);
         $this->call(UsersTableSeeder::class);
     }
 }
